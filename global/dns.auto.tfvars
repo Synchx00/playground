9f22360213ef074ff293470e624261ab6ff38165
Synchx00 <hatta.mailbox00@gmail.com>
2025-10-19 10:08:58 +0000

    adding aws IAM Roles and Route53 Zones

---
+++ global/dns.auto.tfvars
@@ -0,0 +1,1 @@
+domain = "aws.itsecasia.cloud" // @CHANGEME
--- global/providers.tf
+++ global/providers.tf
@@ -18,3 +18,33 @@ provider "aws" {
   # Any region can be set here as IAM is a global service
   region = var.region
 }
+
+# Provider configuration for Assuming Administrator Role in Identity account.
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${module.identity_account.id}:role/Administrator"
+  }
+
+  alias  = "identity"
+  region = var.region
+}
+
+# Provider configuration for Assuming Administrator Role in Prod account.
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${module.prod_account.id}:role/Administrator"
+  }
+
+  alias  = "prod"
+  region = var.region
+}
+
+# Provider configuration for Assuming Administrator Role in Dev account.
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${module.dev_account.id}:role/Administrator"
+  }
+
+  alias  = "dev"
+  region = var.region
+}
