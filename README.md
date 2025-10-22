da8a48e50a1083d75a275ad180de76284c2522ca
Synchx00 <hatta.mailbox00@gmail.com>
2025-10-22 16:22:53 +0000

    installing netpol

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This Project is part of the S4CP Training accessible at [https://rohitsalecha.com/s4cp/](https://rohitsalecha.com/s4cp/)
 
+## Chapter 5I
+
+- Installing Network Security Policy
+
 ## Chapter 5G
 
 - Installing Calico
--- infra/calico.tf
+++ infra/calico.tf
@@ -40,3 +40,52 @@ resource "kubernetes_network_policy" "default_deny" {
   depends_on = [resource.helm_release.calico]
 
 }
+
+# Allow Only ports 8080 and 5432 in ingress. Egress All
+resource "kubernetes_network_policy" "allow_http_pgsql" {
+  metadata {
+    name      = "allow-http-pgsql"
+    namespace = kubernetes_namespace_v1.app.metadata.0.name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {
+        app = var.org_name
+      }
+    }
+    ingress {
+      ports {
+        port     = "8080"
+        protocol = "TCP"
+      }
+      ports {
+        port     = "5432"
+        protocol = "TCP"
+      }
+    }
+    egress {}
+    policy_types = ["Ingress", "Egress"]
+  }
+}
+
+# Allow All Policy
+# resource "kubernetes_network_policy" "allow_all" {
+#   metadata {
+#     name      = "allow-all"
+#     namespace = kubernetes_namespace_v1.app.metadata.0.name
+#   }
+
+#   spec {
+#     pod_selector {
+#       match_labels = {}
+#     }
+#     ingress {}
+#     egress {}
+
+#     policy_types = ["Ingress", "Egress"]
+
+#   }
+
+# }
+
